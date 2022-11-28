4dfd9cdba8892642b09313dcb37fb96d1b59da07
Leandro Gessner <leandrorafaelkg@gmail.com>
2022-11-28 19:41:43 -0300

    New code challenge resolved

--- Home/changing_direction.py
+++ Home/changing_direction.py
@@ -1,25 +1,28 @@
-import enum
-
-
-def changing_direction(elements: list) -> int:
+def changing_direction(elements: list[int]) -> int:
+    direction = ''
     counter = 0
-    
-    for index, number in enumerate(elements):
-        if ((index > 0) and (elements[index - 1] > elements[index])) and (index < len(elements) and (elements[index + 1] < elements[index])):
-            counter += 1
-        
-        print(number)
+
+    for id, val in enumerate(elements):
+        if id < len(elements) - 1:
+            if val > elements[id + 1] and direction == 'up':
+                counter += 1
+            elif val < elements[id + 1] and direction == 'down':
+                counter += 1
+            
+            if val > elements[id + 1]:
+                direction = 'down'
+            elif val < elements[id + 1]:
+                direction = 'up'
+            
 
     return counter
 
 
-# print("Example:")
-# print(changing_direction([1, 2, 3, 4, 5]))
+print("Example:")
+print(changing_direction([1, 2, 3, 4, 5]))
 
-# assert changing_direction([1, 2, 3, 4, 5]) == 0
-# assert changing_direction([1, 2, 3, 2, 1]) == 1
-# assert changing_direction([1, 2, 2, 1, 2, 2]) == 2
+assert changing_direction([1, 2, 3, 4, 5]) == 0
+assert changing_direction([1, 2, 3, 2, 1]) == 1
+assert changing_direction([1, 2, 2, 1, 2, 2]) == 2
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
-
-changing_direction([1, 2, 3, 2, 1])
