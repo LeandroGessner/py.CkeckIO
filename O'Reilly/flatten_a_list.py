8b4831d1dd8fefaf10c0d8b2cd1e57009e837e3b
Leandro Gessner <leandrorafaelkg@gmail.com>
2022-11-27 21:50:22 -0300

    New codes

---
+++ O'Reilly/flatten_a_list.py
@@ -0,0 +1,18 @@
+def flat_list(array):
+    final_list = []
+    
+    for item in array:
+        if isinstance(item, list):
+            final_list.extend(flat_list(item))
+        else:
+            final_list.append(item)
+
+    return final_list
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
